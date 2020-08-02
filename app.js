d3d49689e83636e7ca97a477b56f1862e05bc35a
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-08-02 13:59:03 +0200

    created the bombs and some interesting stuffs

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let width = 10
+    let bombAmount = 20
     let squares = []
 
     //create board
     function createBoard() {
         //get shuffled game array with random bombs
-        
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('valid')
+        console.log(bombsArray)
+        console.log(emptyArray)
+        const gameArray = emptyArray.concat(bombsArray)
+        console.log(gameArray)
 
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
@@ -15,4 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square)
         }
     }
+
+    createBoard()
 })
