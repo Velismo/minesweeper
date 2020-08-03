034294d45f0eb7fcf1bdc619829c69332fb31466
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-08-03 17:05:59 +0200

    adding some numbers

--- app.js
+++ app.js
@@ -15,10 +15,31 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
+            square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
         }
-    }
 
+        //add numbers
+        for(let i = 0; i < squares.length; i++) {
+            let total = 0
+            const isLeftEdge = (i % width === 0)
+            const isRightEdge = (i === width -1)
+
+            if (squares[i].classList.contains('valid')) {
+                if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total++
+                if (i > 9 && !isRightEdge && squares[i + 1 -width].classList.contains('bomb')) total++
+                if (i > 10 && squares[i - width].classList.contains('bomb')) total++
+                if (i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total++
+                if (i < 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total++
+                if (i < 90 && !isLeftEdge && squares[i -1 +width].classList.contains('bomb')) total++
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++
+                if (i < 89 && squares[i + width].classList.contains('bomb')) total++
+                squares[i].setAttribute('data', total)
+            }
+        }
+
+
+    }
     createBoard()
 })
