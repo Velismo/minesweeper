311145d177939ec1257b3163d53986c371327aec
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-08-03 16:42:23 +0200

    created some new const

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
         //get shuffled game array with random bombs
         const bombsArray = Array(bombAmount).fill('bomb')
         const emptyArray = Array(width*width - bombAmount).fill('valid')
-        console.log(bombsArray)
-        console.log(emptyArray)
         const gameArray = emptyArray.concat(bombsArray)
-        console.log(gameArray)
+        const shuffledArray = gameArray.sort(() => Math.random() -0.5)
 
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
